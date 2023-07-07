e4ecd0a62d5f4b07d64d4135f5ab9b86c393bea6
Gulab Singh <gulabsingh.sikarwar@internal.utradesolutions.com>
2023-07-07 17:57:39 +0530

    added material library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule, 
+    MatFormFieldModule, 
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { MatSelectModule } from '@angular/material/select'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent {
+
+}
